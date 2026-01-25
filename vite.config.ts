9c6f2cda29ad0d2f56e61cf44d353596da5cc867
meerramis-blip <meerramis@gmail.com>
2026-01-25 12:24:17 +0530

    Update base path in Vite configuration
    Change base path for deployment to root.

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
 
   return {
-    base: "/Meerra-Mevawala-Website/",
+    base: "/",
 
     server: {
       port: 3000,
