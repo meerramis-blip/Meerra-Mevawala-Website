48133b1d5ba9a546397ed7e79f4562f8f2a2892f
meerramis-blip <meerramis@gmail.com>
2026-01-26 23:22:34 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,22 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-const Test = () => {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Site is working</h1>
-      <p>If you see this, routing and Netlify are correct.</p>
-    </div>
-  );
-};
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Test />} />
-        <Route path="*" element={<Test />} />
-      </Routes>
-    </BrowserRouter>
+    <div style={{ padding: 40 }}>
+      <h1>App loaded</h1>
+      <p>If you see this, App.tsx itself is not crashing.</p>
+    </div>
   );
 }
