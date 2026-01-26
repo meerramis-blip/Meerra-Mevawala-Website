088b25fa236730c1d142edb24eb31384522605b1
meerramis-blip <meerramis@gmail.com>
2026-01-26 23:47:33 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,50 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import CookieConsent from "./components/CookieConsent";
+import ScrollToTopButton from "./components/ScrollToTopButton";
+
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Academy from "./pages/Academy";
+import Portfolio from "./pages/Portfolio";
+import Blog from "./pages/Blog";
+import Booking from "./pages/Booking";
+import Contact from "./pages/Contact";
+import Collaborations from "./pages/Collaborations";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsAndConditions from "./pages/TermsAndConditions";
+import CookiePolicy from "./pages/CookiePolicy";
 
 export default function App() {
   return (
-    <div style={{ padding: 20 }}>
-      <h1>Testing Home page</h1>
-      <Home />
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/academy" element={<Academy />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/collaborations" element={<Collaborations />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+          <Route path="/cookie-policy" element={<CookiePolicy />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </main>
+
+      <Footer />
+      <CookieConsent />
+      <ScrollToTopButton />
     </div>
   );
 }
