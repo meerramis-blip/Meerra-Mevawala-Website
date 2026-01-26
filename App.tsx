d502ee38c69d9002a5b91537f885547000a4fd1e
meerramis-blip <meerramis@gmail.com>
2026-01-26 23:08:03 +0530

    Update Layout component structure
    Refactor Layout component structure and remove Navbar, Footer, CookieConsent, and ScrollToTopButton.

--- App.tsx
+++ App.tsx
@@ -33,13 +33,8 @@ const ScrollToTopOnNavigate = () => {
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-grow">{children}</main>
-      <Footer />
-      <CookieConsent />
-      {/* <EditorGuide /> */}
-      <ScrollToTopButton />
+    <div className="min-h-screen">
+      {children}
     </div>
   );
 };
