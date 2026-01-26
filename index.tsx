8e942d99e1fa5b4a80383599a23847c8e7ddf47b
meerramis-blip <meerramis@gmail.com>
2026-01-26 23:47:07 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import { StoreProvider } from "./store";
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -12,8 +13,9 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <StoreProvider>
+        <App />
+      </StoreProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
- 
