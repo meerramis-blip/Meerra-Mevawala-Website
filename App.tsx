ca0bf034c78bcb23dadaf9d15b3a14a7c83d3bf3
meerramis-blip <meerramis@gmail.com>
2026-01-26 23:43:03 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Academy from "./pages/Academy";
+import Portfolio from "./pages/Portfolio";
+import Blog from "./pages/Blog";
+import Booking from "./pages/Booking";
+import Contact from "./pages/Contact";
+import Collaborations from "./pages/Collaborations";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsAndConditions from "./pages/TermsAndConditions";
+import CookiePolicy from "./pages/CookiePolicy";
 
 export default function App() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>App loaded</h1>
-      <p>If you see this, App.tsx itself is not crashing.</p>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/academy" element={<Academy />} />
+      <Route path="/portfolio" element={<Portfolio />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/collaborations" element={<Collaborations />} />
+      <Route path="/booking" element={<Booking />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+      <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+      <Route path="/cookie-policy" element={<CookiePolicy />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 }
- 
