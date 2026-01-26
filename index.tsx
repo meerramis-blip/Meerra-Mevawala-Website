623c910204605c3fadedc58cbf2169b1d3a5c4ed
meerramis-blip <meerramis@gmail.com>
2026-01-26 23:18:54 +0530

    Refactor index.tsx to simplify rendering
    Removed BrowserRouter and App components, added debug message.

--- index.tsx
+++ index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-import App from './App';
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  document.body.innerHTML = "<h1>ROOT NOT FOUND</h1>";
+  throw new Error("Could not find root element");
 }
+
+document.body.insertAdjacentHTML("beforeend", "<div id='debug' style='padding:20px;color:red'>JS Loaded</div>");
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <div style={{ padding: 40 }}>
+    <h1>React is running</h1>
+    <p>If you see this, React mounting is working.</p>
+  </div>
 );
