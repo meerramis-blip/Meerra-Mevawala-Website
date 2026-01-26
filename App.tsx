42abc93e34410d12bf3cc4e2551acbff9e83722c
meerramis-blip <meerramis@gmail.com>
2026-01-26 23:11:18 +0530

    Refactor App component and routing structure

--- App.tsx
+++ App.tsx
@@ -1,73 +1,22 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { StoreProvider } from './store';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import CookieConsent from './components/CookieConsent';
-import EditorGuide from './components/EditorGuide';
-import ScrollToTopButton from './components/ScrollToTopButton';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Academy from './pages/Academy';
-import Portfolio from './pages/Portfolio';
-import Blog from './pages/Blog';
-import Booking from './pages/Booking';
-import Contact from './pages/Contact';
-import Collaborations from './pages/Collaborations';
-import PrivacyPolicy from './pages/PrivacyPolicy';
-import TermsAndConditions from './pages/TermsAndConditions';
-import CookiePolicy from './pages/CookiePolicy';
-
-/* Scroll to top on route change */
-const ScrollToTopOnNavigate = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-};
-
-const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const Test = () => {
   return (
-    <div className="min-h-screen">
-      {children}
+    <div style={{ padding: 40 }}>
+      <h1>Site is working</h1>
+      <p>If you see this, routing and Netlify are correct.</p>
     </div>
   );
 };
 
-const App: React.FC = () => {
+export default function App() {
   return (
-    <StoreProvider>
-      <Router>
-        <ScrollToTopOnNavigate />
-
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/academy" element={<Academy />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/collaborations" element={<Collaborations />} />
-            <Route path="/booking" element={<Booking />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-            <Route path="/cookie-policy" element={<CookiePolicy />} />
-
-            {/* Fallback */}
-            <Route path="*" element={<Home />} />
-          </Routes>
-        </Layout>
-
-      </Router>
-    </StoreProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Test />} />
+        <Route path="*" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
   );
-};
-
-export default App;
+}
